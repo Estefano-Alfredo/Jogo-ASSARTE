a2cb822612bbbeb35a6b667bbc58e821042b0048
SrKem <kassiomori@gmail.com>
2025-11-10 21:06:41 -0300

    NIVEL 2

--- bio-geometria/bio-geometria/Nos/carta.gd
+++ bio-geometria/bio-geometria/Nos/carta.gd
@@ -4,13 +4,35 @@ extends Control
 @onready var anim: AnimationPlayer = $AnimationPlayer
 
 @export var valor_da_carta : int
+
 var amostra := false
 var correto := false
+
+const ABELHA = preload("uid://cysxxnij258ql")
+const ARARA = preload("uid://du2qxw3r8dyx1")
+const LOBO = preload("uid://duoljuw5ie3sc")
+const PANDA = preload("uid://bcjgxf86qujjv")
+const TARTARUGA = preload("uid://d17lvrv7wxxrh")
+
+@onready var texture: TextureRect = $TextureRect
+
 signal teste
 
-#func _process(delta: float) -> void:
-	#text = str(valor_da_carta)
-	#pass
+var conf := false
+func _process(_delta: float) -> void:
+	if conf	== true:
+		return
+	match valor_da_carta:
+		1:
+			texture.texture = ABELHA
+		2:
+			texture.texture = ARARA
+		3:
+			texture.texture = LOBO
+		4:
+			texture.texture = PANDA
+		5:
+			texture.texture = TARTARUGA
 
 func esconder_carta():
 	anim.play_backwards("flip")
